98f2a6d71a6cc8e4ce3763200a5a0cccef28c01f
robsonfagundes <robsonfagundes@gmail.com>
2015-12-07 20:13:51 -0200

    Cap2 - pg10, Ver2

--- cap2/server.js
+++ cap2/server.js
@@ -1,2 +1,2 @@
 var http = require('http');
-http.createServer("Here enter the Express and his middlewares!\n").listen(3000, 127,0,0,1);
+http.createServer('Here enter the Express and his middlewares!\n').listen(3000, 127,0,0,1);
