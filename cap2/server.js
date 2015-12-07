8dd2d03d7cc05672f6a2d3b462a02686d158210f
robsonfagundes <robsonfagundes@gmail.com>
2015-12-07 20:10:59 -0200

    Cap2 - pg10, Ver2

--- cap2/server.js
+++ cap2/server.js
@@ -1,5 +1,2 @@
 var http = require('http');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type' : 'text/plain'});
-	res.end("I'm a server created in Node.js!\n");
-}).listen(3000, 127,0,0,1);
+http.createServer("Here enter the Express and his middlewares!\n").listen(3000, 127,0,0,1);
