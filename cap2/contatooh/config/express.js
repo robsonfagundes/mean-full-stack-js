a6e7f9919948a164ac90fdc026d9ffc01fd43b50
robsonfagundes <robsonfagundes@gmail.com>
2015-12-14 21:51:41 -0200

    Capítulo 2 - framework para Node.js - FIM

--- cap2/contatooh/config/express.js
+++ cap2/contatooh/config/express.js
@@ -1,6 +1,7 @@
 // config/express.js
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser')
 
 module.exports = function(){
 	var app = express();
@@ -15,7 +16,14 @@ module.exports = function(){
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
 
-	// express-load
+	// middleware for http protocol (put and delete) --> parse application/x-www-form-urlencoded 
+	app.use(bodyParser.urlencoded({ extended: false }))
+	// parse application/json 
+	app.use(bodyParser.json())
+
+	app.use(require('method-override')());
+
+	// load routes (express-load)
 	load('models', {cwd: 'app'})
 		.then('controllers')
 		.then('routes')
