b3d844e1d320183b6fdda944f4b4a7ac530af66d
Robson Fagundes <robsonfagundes@gmail.com>
2015-12-15 14:53:23 -0200

    Filtering Name on Static Contact List - Directive ng-repeat='contact in contatos | filter: name_filter'

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -15,5 +15,8 @@
         			{'_id': 2, 'name': 'robson r-sys', 'email': 'fagundes@rsyssoftwares.com.br'},
         			{'_id': 3, 'name': 'r-sys softwares', 'email': 'contato@rsyssoftwares.com.br'}
         		];
+        		//filter by name and email
+        		$scope.name_filter = '';
+        		
         	});
 })();
