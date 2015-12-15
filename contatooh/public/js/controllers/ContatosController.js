f321cca98fa71ed89363f69db96759fd26b62c22
Robson Fagundes <robsonfagundes@gmail.com>
2015-12-15 14:45:48 -0200

    Adding Static Contact List - Directive ng-repeat

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -9,13 +9,11 @@
         		$scope.add = function() {
         			$scope.total++;
         		} 
-
         		//list contacts
         		$scope.contatos = [
         			{'_id': 1, 'name': 'robson fagundes', 'email': 'robsonfagundes@gmail.com'},
         			{'_id': 2, 'name': 'robson r-sys', 'email': 'fagundes@rsyssoftwares.com.br'},
         			{'_id': 3, 'name': 'r-sys softwares', 'email': 'contato@rsyssoftwares.com.br'}
         		];
-
         	});
 })();
