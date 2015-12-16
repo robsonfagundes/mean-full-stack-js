2b156d30e1b5feb20ab217ebae1f555133358945
Robson Fagundes <robsonfagundes@gmail.com>
2015-12-16 10:21:18 -0200

    Capitulo 5 - Integrating AngularJS e Express

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -3,20 +3,20 @@
 
     angular
         .module('contatooh')
-        	.controller('ContatosController', function($scope) {
-        		//add
-        		$scope.total = 0;
-        		$scope.add = function() {
-        			$scope.total++;
-        		} 
-        		//list contacts
-        		$scope.contatos = [
-        			{'_id': 1, 'name': 'robson fagundes', 'email': 'robsonfagundes@gmail.com'},
-        			{'_id': 2, 'name': 'robson r-sys', 'email': 'fagundes@rsyssoftwares.com.br'},
-        			{'_id': 3, 'name': 'r-sys softwares', 'email': 'contato@rsyssoftwares.com.br'}
-        		];
-        		//filter by name and email
-        		$scope.name_filter = '';
-        		
-        	});
+    	.controller('ContatosController', function($scope, $http) {
+    		
+    		// fields	
+    		$scope.contacts = [];
+    		$scope.filter = '';
+			
+			// list all coontacts        		
+    		$http.get('/contatos')
+    			.success(function(data) {
+    				$scope.contacts = data;
+    			})
+    			.error(function(statusText) {
+    				console.log('Do not is possible get the list of contacts!')
+    			});
+    		
+    	});
 })();
