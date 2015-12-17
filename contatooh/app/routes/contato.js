89ed50a4a7d8a81d1d1c9eeef1ce02eb7cfda9b2
Robson Fagundes <robsonfagundes@gmail.com>
2015-12-17 11:00:46 -0200

    Capitulo 5 - Integrating AngularJS e Express

--- contatooh/app/routes/contato.js
+++ contatooh/app/routes/contato.js
@@ -3,7 +3,8 @@ module.exports = function(app) {
 	var controller = app.controllers.contato;
 
 	app.route('/contatos')
-		.get(controller.listaContatos);
+		.get(controller.listaContatos)
+		.post(controller.salvaContato);
 
 	app.route('/contatos/:id')
 		.get(controller.obtemContato)
--- contatooh/public/js/controllers/ContatoController.js
+++ contatooh/public/js/controllers/ContatoController.js
@@ -3,7 +3,41 @@
 
     angular
         .module('contatooh')
-    	.controller('ContatoController', function($scope, $routeParams) {
-    		console.log('The contact is: ' + $routeParams.contatoId);
+    	.controller('ContatoController',
+
+    		function($scope, $routeParams, Contact) {
+				
+    			// obj
+    			$scope.msg = {text: ''};
+	
+    			// get obj contact
+				if($routeParams.contatoId){
+					Contact.get({id: $routeParams.contatoId},
+					function(contact) {
+						$scope.contact = contact;
+						console.log('The contact is: ' + $routeParams.contatoId);
+					},
+					function(error) {
+						$scope.msg = {text: 'Do not is possible get the contact!'};
+						console.log(error);
+					}
+				);
+				} else {
+					$scope.contact = {}
+				}
+
+				// save contact
+				$scope.saveContact = function() {
+					$scope.contact.$save()
+						.then(function () {
+							$scope.msg = {text: 'Contact Saved!'};
+							$scope.contact = new Contato(); //clear form
+						})
+						.catch(function (error) {
+							$scope.msg = {text: 'Do not is possible add contact on list!'};
+							console.log(error);
+						});
+				};
+
     	});
 })();
